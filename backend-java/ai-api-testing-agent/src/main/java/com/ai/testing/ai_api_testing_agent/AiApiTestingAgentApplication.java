a80fd161c0ded3a88cfd73ad640ef6433958aba3
mdhasan <ext.mdhasan13606@xpressbees.com>
2026-05-11 10:41:33 +0530

    ai-agent first commit

---
+++ backend-java/ai-api-testing-agent/src/main/java/com/ai/testing/ai_api_testing_agent/AiApiTestingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ai.testing.ai_api_testing_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApiTestingAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiApiTestingAgentApplication.class, args);
+	}
+
+}
---
+++ backend-java/ai-api-testing-agent/src/test/java/com/ai/testing/ai_api_testing_agent/AiApiTestingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.testing.ai_api_testing_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApiTestingAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
